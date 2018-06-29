afc0d807f5652d46a82a7f64c850d9e747cd0321
Lzzya <lzzy201311@163.com>
2018-06-29 11:01:26 +0800

    增加计算成本的代码
    1. 增加判断能否回到配送站的约束
    2. 增加计算成本的代码，以及为此需要的数据摘取和计算

--- entity/TransportPath.py
+++ entity/TransportPath.py
@@ -1,8 +1,13 @@
 class TransportPath(object):
 
-    def __init__(self, path, vehicle_id):
+    def __init__(self, path, vehicle_id, wating_tm, start_tm, back_tm,distance, charge_cost, weight):
         self.path = path
         self.vehicle_id = vehicle_id
-
+        self.weight = weight
+        self.wating_tm = wating_tm
+        self.distance = distance
+        self.start_tm = start_tm
+        self.back_tm = back_tm
+        self.charge_cost = charge_cost
     def calc_path_info(self, distance_matrix, time_matrix, vehicle_info):
         pass
